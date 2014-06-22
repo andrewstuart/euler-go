3d5264d2e6637157fd76e444eed008fce889d618
Andrew Stuart <andrew.stuart2@gmail.com>
2014-06-21 20:50:53 -0700

    Solve euler 4

---
+++ 004/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//String reverser
+func rev(s string) string {
+	a := make([]byte, len(s))
+
+	for i := len(s); i > 0; i-- {
+		a[len(s)-i] = s[i-1]
+	}
+
+	return string(a)
+}
+
+func isPalindrome(n int) bool {
+	nStr := strconv.Itoa(n)
+
+	nRev := rev(nStr)
+
+	return nStr == nRev
+}
+
+func main() {
+	largest := 0
+
+	for i := 999; i >= 100; i-- {
+		for j := 999; j >= 100; j-- {
+			test := i * j
+
+			if isPalindrome(test) && test > largest {
+				largest = test
+			}
+		}
+	}
+
+	fmt.Println(largest)
+}
