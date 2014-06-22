6868a9e10cf065b93bf9d090e1dd0f0fb4a1a15c
Andrew Stuart <andrew.stuart2@gmail.com>
2014-06-22 07:48:10 -0700

    Solve problem 7

--- 007/main.go
+++ 007/main.go
@@ -1,14 +1,22 @@
 package main
 
+import (
+	"euler/common"
+	"fmt"
+)
+
 // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 // What is the 10 001st prime number?
 
-import (
-	eu "euler/004"
-	"fmt"
-)
-
 func main() {
-	fmt.Println(eu.IsPrime(3))
+	primes := []int{2}
+
+	for i := 3; len(primes) <= 10001; i += 2 {
+		if common.IsPrime(float64(i)) {
+			primes = append(primes, i)
+		}
+	}
+
+	fmt.Println(primes[10000])
 }
