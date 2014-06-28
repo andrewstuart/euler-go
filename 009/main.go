18dfd469a440559877a4e8a74317c9dca7a9cedd
Andrew Stuart <andrew.stuart2@gmail.com>
2014-06-28 10:44:00 -0700

    Solve problem 9

--- 009/main.go
+++ 009/main.go
@@ -7,4 +7,24 @@
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
 
+package main
 
+import "fmt"
+
+//a=m^2-n^2, b=2mn, c=m^2+n^2
+
+func main() {
+	sum := 0
+	var a, b, c int
+	for m := 1; sum != 1000; m++ {
+		for n := 1; n < m && sum != 1000; n++ {
+
+			a = m*m - n*n
+			b = 2 * m * n
+			c = m*m + n*n
+
+			sum = a + b + c
+		}
+	}
+	fmt.Println(a * b * c)
+}
